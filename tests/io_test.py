5dce5b54e4ff13a2a3491f12a2c7da6c6c01c76e
WeiwenXu21 <wx37523@uga.edu>
2018-04-04 00:42:04 -0400

    Added low level io and test

---
+++ tests/io_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import edamame.io_support as io
+
+path = '../data/train_1.csv'
+
+def test_load_data():
+    
+    page, date, data = io.load_data(path)
+    
+    assert np.shape(page) == (145063,)
+    assert np.shape(date) == (550,)
+    assert np.shape(data) == (145063, 550)
