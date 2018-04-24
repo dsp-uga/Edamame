099e6cd50c9fa1ae256891ce9e32d74ebb03f12f
melanie <ihuei.ho25@uga.edu>
2018-04-23 20:42:58 -0400

    update __init__.py

--- ARIMA/__init__.py
+++ ARIMA/__init__.py
@@ -1,5 +1,5 @@
 from . import arima_model, itsmpy, data_preprocessing
-# from ..support import evaluation, io_support
+from ..support import evaluation, io_support
 from .arima_model import fit_arima_model
 from .itsmpy import ITSM
 from ..support.io_support import load_data
