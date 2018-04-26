4b68a689d330a8c0fc7d1a033df91c9b4947550c
I-Huei Ho <ihuei.ho25@uga.edu>
2018-04-26 19:17:16 -0400

    call requirement.txt
    then requirement.txt

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 from setuptools import setup
-setup(packages=['ARIMA','LSTM'])
+required = open('requirements.txt').read().split('\n')
+setup(
+  packages = ['ARIMA','LSTM'],
+  install_requires = required
+)
